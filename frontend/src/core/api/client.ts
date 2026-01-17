a87f4b2d818936ec74e1b8b7577e97a3d3576db2
Hafidz Rizky <hafidzrizky864@gmail.com>
2026-01-17 09:50:46 +0700

    Move to Vercel deployment

--- frontend/src/core/api/client.ts
+++ frontend/src/core/api/client.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 // Kita buat instance axios khusus
 const apiClient = axios.create({
-  baseURL: 'http://127.0.0.1:8000', // Alamat Backend Python Anda
+  baseURL: 'https://petroapp-engine-9yfragqvfz9zwgkuhgxkrx.streamlit.app', // Alamat Backend Python Anda
   headers: {
     'Content-Type': 'application/json',
   },

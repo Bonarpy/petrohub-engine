8ddaf261a24954682e242cbe7d581528fb4b3efe
Hafidz Rizky <hafidzrizky864@gmail.com>
2026-01-17 10:36:33 +0700

    Fix CORS: Open access for Frontend

--- app/main.py
+++ app/main.py
@@ -106,7 +106,7 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,      # Izinkan list di atas
+    allow_origins=["*"],      # Izinkan list di atas
     allow_credentials=True,
     allow_methods=["*"],        # Izinkan semua method (GET, POST, dll)
     allow_headers=["*"],        # Izinkan semua header

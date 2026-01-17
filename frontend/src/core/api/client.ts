cbff10af618d5e7459b9ac0903a9fa6eba7f1f4a
Hafidz Rizky <hafidzrizky864@gmail.com>
2026-01-17 11:47:24 +0700

    fix: update backend baseURL to petrohub-engine

--- frontend/src/core/api/client.ts
+++ frontend/src/core/api/client.ts
@@ -1,9 +1,9 @@
 // src/core/api/client.ts
 import axios from 'axios';
 
-// Kita buat instance axios khusus
+// Kita buat instance axios khusus mengarah ke BE Engine yang baru
 const apiClient = axios.create({
-  baseURL: 'https://petrohub.vercel.app', // Alamat Backend Python Anda
+  baseURL: 'https://petrohub-engine.vercel.app', 
   headers: {
     'Content-Type': 'application/json',
   },

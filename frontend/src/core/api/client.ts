f37f538584701ea3c9f4253938a8beb17b9b4d85
Hafidz Rizky <hafidzrizky864@gmail.com>
2026-01-24 15:45:13 +0700

    Deploy week 3

--- frontend/src/core/api/client.ts
+++ frontend/src/core/api/client.ts
@@ -3,8 +3,8 @@ import axios from 'axios';
 
 // Kita buat instance axios khusus mengarah ke BE Engine yang baru
 const apiClient = axios.create({
-  // baseURL: 'https://petrohub-engine.vercel.app'
-  baseURL: 'http://127.0.0.1:8000',
+  baseURL: 'https://petrohub-engine.vercel.app',
+  // baseURL: 'http://127.0.0.1:8000',
   headers: {
     'Content-Type': 'application/json',
   },
